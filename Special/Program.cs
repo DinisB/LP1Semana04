e30c52c553b34a4c9818ad4483a38f895bfd0a77
DinisB <dinisbarroso.dev@gmail.com>
2025-03-13 15:42:47 +0000

    Exercicio 4 - Special

---
+++ Special/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Special
+{
+    public class Program
+    {
+        private static int GetSpecial(int n)
+        {
+            if (n == 0) return 0;
+            if (n == 1) return 1;
+            return GetSpecial(n - 1) + 2 * GetSpecial(n - 2);
+        }
+        private static void Main(string[] args)
+        {
+            Console.Write("Insert n-th number of the special sequence: ");
+            int.TryParse(Console.ReadLine(), out int num);
+            Console.WriteLine(GetSpecial(num));
+        }
+    }
+}
