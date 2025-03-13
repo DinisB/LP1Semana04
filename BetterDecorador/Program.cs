b9bca7ae60c35ed4a06999adf91e5829d11c19a9
DinisB <dinisbarroso.dev@gmail.com>
2025-03-13 14:54:11 +0000

    Exercicio 3 - Sem comentários

--- BetterDecorador/Program.cs
+++ BetterDecorador/Program.cs
@@ -6,25 +6,32 @@ namespace BetterDecorador
     public class Program
     {
         /// <summary>
-            /// Decorate string
-            /// </summary>
-            /// <param name="s">String to be used </param>
-            /// <param name="dec">Symbol to decorate </param>
-            /// <param name="num">Number of times for symbol to appear </param>
-            /// <returns> Decorated String </returns>
+        /// Decorate string
+        /// </summary>
+        /// <param name="s">String to be used </param>
+        /// <param name="dec">Symbol to decorate </param>
+        /// <param name="num">Number of times for symbol to appear </param>
+        /// <returns> Decorated String </returns>
         private static string Decor(string s, char dec, int num)
         {
             string res = new String(dec, num);
             return $"{res} {s} {res}";
         }
         /// <summary>
-            /// Decorate string
-            /// </summary>
-            /// <param name="args">Console arguments </param>
-            /// <returns> Print decorated string to console </returns>
+        /// Main method
+        /// </summary>
+        /// <param name="args">Console arguments </param>
+        /// <returns> Print decorated string to console </returns>
         private static void Main(string[] args)
         {
-            Console.WriteLine(Decor(args[0], Convert.ToChar(args[1]), Convert.ToInt32(args[2])));
+            if (args.Length != 0)
+            {
+                Console.WriteLine(Decor(args[0], Convert.ToChar(args[1]), Convert.ToInt32(args[2])));
+            }
+
+            if (args.Length==0) {
+                Console.WriteLine(Decor("User did not specify args !", '=', 3));
+            }
         }
     }
 }
