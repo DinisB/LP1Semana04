00cbe77cab989cd6340cbdfa0e43e012fa27da1a
DinisB <dinisbarroso.dev@gmail.com>
2025-03-13 14:28:33 +0000

    BetterDecorador

---
+++ BetterDecorador/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+namespace BetterDecorador
+{
+    public class Program
+    {
+        private static string Decor(string s, char dec, int num)
+        {
+            string res = new String(dec, num);
+            return $"{res} {s} {res}";
+        }
+        private static void Main(string[] args)
+        {
+            Console.WriteLine(Decor(args[0], Convert.ToChar(args[1]), Convert.ToInt32(args[2])));
+        }
+    }
+}
