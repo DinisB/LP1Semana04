3ff726c76c671c5eef5ebae4994f5a2a05a4277c
DinisB <dinisbarroso.dev@gmail.com>
2025-03-13 14:34:29 +0000

    Exercicio 2 - BetterDecorador

--- BetterDecorador/Program.cs
+++ BetterDecorador/Program.cs
@@ -5,11 +5,23 @@ namespace BetterDecorador
 {
     public class Program
     {
+        /// <summary>
+            /// Decorate string
+            /// </summary>
+            /// <param name="s">String to be used </param>
+            /// <param name="dec">Symbol to decorate </param>
+            /// <param name="num">Number of times for symbol to appear </param>
+            /// <returns> Decorated String </returns>
         private static string Decor(string s, char dec, int num)
         {
             string res = new String(dec, num);
             return $"{res} {s} {res}";
         }
+        /// <summary>
+            /// Decorate string
+            /// </summary>
+            /// <param name="args">Console arguments </param>
+            /// <returns> Print decorated string to console </returns>
         private static void Main(string[] args)
         {
             Console.WriteLine(Decor(args[0], Convert.ToChar(args[1]), Convert.ToInt32(args[2])));
